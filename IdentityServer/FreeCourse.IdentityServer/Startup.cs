60972914dba7053cbf6e2039c0e85052aed08a57
mbrnkr <birankarmurat@gmail.com>
2022-02-01 23:26:43 +0300

    IdentityServer : Signup controller Authorize

--- IdentityServer/FreeCourse.IdentityServer/Startup.cs
+++ IdentityServer/FreeCourse.IdentityServer/Startup.cs
@@ -28,6 +28,9 @@ namespace FreeCourse.IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddLocalApiAuthentication();
+
             services.AddControllersWithViews();
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -81,6 +84,7 @@ namespace FreeCourse.IdentityServer
 
             app.UseRouting();
             app.UseIdentityServer();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
