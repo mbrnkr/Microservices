2cfee4d83850bec0a57fe2092e4a13ac870b0b95
mbrnkr <birankarmurat@gmail.com>
2022-02-28 00:06:17 +0300

    PhotoStock  : authentication added

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,12 +26,19 @@ namespace FreeCourse.Services.PhotoStock
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "photo_stock_catalog";
+                options.RequireHttpsMetadata = false;
+            });
+            services.AddControllers(opt => {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.PhotoStock", Version = "v1" });
-            });
+            });      
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -44,7 +53,9 @@ namespace FreeCourse.Services.PhotoStock
             app.UseStaticFiles();
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseAuthorization();       
 
             app.UseEndpoints(endpoints =>
             {
